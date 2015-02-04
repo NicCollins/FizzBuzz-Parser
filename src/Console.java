30e453432d6e189cb7113d7b4c75cf1eeba8a230
Nicolas Collins <collins.nicolas@gmail.com>
2015-02-03 22:44:00 -0500

    Added page classes and copyright headers

--- src/Console.java
+++ src/Console.java
@@ -1,3 +1,22 @@
+/*
+ * Console application to create output to various Fizz Buzz variants
+ * Copyright (C) 2015.  Nicolas A. Collins
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,7 +34,7 @@ public class Console {
         System.out.println();
         System.out.println("Please choose one of the following options:");
         System.out.println("1) Standard FizzBuzz");
-        System.out.println("2) Isreali FizzBuzz Variant");
+        System.out.println("2) Israeli FizzBuzz Variant");
         System.out.println("3) Custom Variant");
         System.out.print("Selection: ");
         String choice = br.readLine();
